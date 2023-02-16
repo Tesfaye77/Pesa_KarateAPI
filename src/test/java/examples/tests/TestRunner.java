d21b8be68b5e6c02888a27d91e7de0bb6ec893aa
unknown <Tesfaye77>
2023-02-16 17:10:44 +0100

    hello

--- src/test/java/examples/tests/TestRunner.java
+++ src/test/java/examples/tests/TestRunner.java
@@ -1,19 +1,25 @@
 package examples.tests;
 
 
+import cucumber.api.CucumberOptions;
 import org.junit.Test;
 import com.intuit.karate.Results;
 import com.intuit.karate.Runner;
 import net.masterthought.cucumber.Configuration;
 import net.masterthought.cucumber.ReportBuilder;
 import org.apache.commons.io.FileUtils;
+
+
 import java.io.File;
 import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import static junit.framework.TestCase.assertTrue;
 
-
+@CucumberOptions(
+        plugin = { "pretty", "html:target/cucumber-reports","json:target/Cucumber.json" },
+        monochrome = true
+)
 
  public class TestRunner {
 @Test
@@ -44,7 +50,7 @@ public void testParallel() {
         Collection<File> jsonFiles = FileUtils.listFiles(new File(karateOutputPath), new String[] {"json"}, true);
         List<String> jsonPaths = new ArrayList<String>(jsonFiles.size());
         jsonFiles.forEach(file -> jsonPaths.add(file.getAbsolutePath()));
-        Configuration config = new Configuration(new File("target"), "Report-Name");
+        Configuration config = new Configuration(new File("target"), "Z-Train-Report-API");
         ReportBuilder reportBuilder = new ReportBuilder(jsonPaths, config);
         reportBuilder.generateReports();
     }
